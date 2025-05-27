5296afd1dd254a42efde2846d4d35e85f8c30bbc
THarris06 <144939694+THarris06@users.noreply.github.com>
2025-05-27 13:23:58 -0230

    sadsd

--- REACTPROJECT/react-project/src/App.jsx
+++ REACTPROJECT/react-project/src/App.jsx
@@ -17,13 +17,16 @@ function App() {
         </a>
       </div>
       <h1>My React Project</h1>
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      </div> */}
+      <div className='NavBar'>
+        <p>Assignment 1</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
